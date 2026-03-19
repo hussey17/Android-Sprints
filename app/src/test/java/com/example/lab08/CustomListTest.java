68d156bd7e841725172b8cf2bce5174152548e16
hussey17 <27100157@lums.edu.pk>
2026-03-19 13:17:16 +0500

    Implementing hasCity Method --> Green Phase of TDD

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -1,7 +1,22 @@
 package com.example.lab08;
-
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-public class CustomListTest {
 
+public class CustomListTest {
+    @Test
+    public void testHasCity() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        list.addCity(calgary);
+        assertTrue(list.hasCity(calgary));
+    }
+    @Test
+    public void testHasCityNotFound() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        City edmonton = new City("Edmonton", "AB");
+        list.addCity(calgary);
+        // City not in list should return false //
+        assertFalse(list.hasCity(edmonton));
+    }
 }
